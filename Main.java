ffea2944b91e1c56b072d8168690036d700fb2ce
sumukh624-hub <sumukh624@gmail.com>
2026-01-29 23:05:46 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,24 +1,10 @@
 import java.util.Scanner;
 
 public class Main{
-
-    // Method to calculate area of square
-    public static double calculateArea(double side) {
-        
-        return side * side;
-    }
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        
-            System.out.print("Enter the side length of the square: ");
-
-            double side = 8;
-            double area = calculateArea(side);
-
-            System.out.printf("The area of the square with side %.2f is: %.2f%n", side, area);
-
-        }
+        double side = 5.0;
+        double area = side * side;
+        System.out.println("The side length is: " + side);
+        System.out.println("The area of the square is: " + area);
     }
-
+}
